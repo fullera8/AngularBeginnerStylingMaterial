460033ecce10b907ef07ec301f7ced065588ebb3
Alex Fuller <fulleralex47@gmail.com>
2021-04-06 06:10:01 -0400

    rearchitected the material module so all materials are in a shared seperate file.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MaterialModule } from './shared/material.module';
+import{ MaterialModule } from './shared/material.module';
 import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
